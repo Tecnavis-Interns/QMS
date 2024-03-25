89c356c2425d5ba35aacc12a3d13472b8f8f906d
Muhammed Sahal K C <msahalkc@gmail.com>
2024-03-25 14:10:11 +0530

    build: main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,26 @@
 // main.tsx or main.jsx
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {NextUIProvider} from '@nextui-org/react'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { NextUIProvider } from '@nextui-org/react';
+import App from './App';
+import './index.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Link,
+} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <NextUIProvider>
-      <App />
+      <RouterProvider router={router} />
     </NextUIProvider>
   </React.StrictMode>,
-)
+);
