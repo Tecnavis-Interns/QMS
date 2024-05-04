0bc64da218a661a042976dc4a0fd6b36887a4ad4
Prnv2001 <145655913+Prnv2001@users.noreply.github.com>
2024-05-04 21:03:36 +0530

    Update Navbar.jsx

--- src/Counter/Navbar.jsx
+++ src/Counter/Navbar.jsx
@@ -17,31 +17,28 @@ export default function App() {
     navigate("/")
   }
   return (
-    <Navbar>
-      <NavbarContent>
-      <NavbarBrand>
-          <p className="font-bold text-inherit sm:hidden">QMS</p>
-          <p className="font-bold text-inherit hidden sm:block">
+    <div className="flex h-screen">
+      {/* Sidebar */}
+      <aside className="w-64 bg-[#6236F5]">
+        <div className="flex items-center justify-center h-16">
+          <p className="font-bold text-white hidden sm:block">
             Queue Management System
           </p>
-        </NavbarBrand>
-      </NavbarContent>
-      <NavbarContent className="!justify-center">
-        <p>Counter Dashboard</p>
-      </NavbarContent>
-      <NavbarContent justify="end">
-        <NavbarItem>
+          <p className="font-bold text-white sm:hidden">QMS</p>
+        </div>
+        <div className="flex flex-col items-center justify-between flex-1 p-4">
+          <p className="text-white">Counter Dashboard</p>
           <Button
             as={Link}
-            className="bg-[#6236F5] text-white"
+            className="w-full mt-4 bg-gray-900 text-white"
             href="#"
             variant="flat"
             onClick={handleLogout}
           >
             Logout
           </Button>
-        </NavbarItem>
-      </NavbarContent>
-    </Navbar>
-  );
+        </div>
+      </aside>
+    </div>
+  );
 }
