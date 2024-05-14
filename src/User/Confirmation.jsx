e67ec7119e1b4cd537b82a49108aecfe7640685f
Shamal Khalid N P <shamalkhalidnp@gmail.com>
2024-05-15 01:01:58 +0530

    Updated Token Generation in userForm

---
+++ src/User/Confirmation.jsx
@@ -0,0 +1,59 @@
+import { useEffect } from "react";
+import { Button } from "@nextui-org/react";
+import { useNavigate, useLocation } from "react-router-dom";
+import { PDFDocument, rgb } from 'pdf-lib';
+
+export default function ConfirmationPage() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { tokenNumber } = location.state; // Retrieve tokenNumber from location state
+
+  useEffect(() => {
+    // Scroll to the top of the page when the component mounts
+    window.scrollTo(0, 0);
+  }, []);
+
+  const handleDownloadPDF = async () => {
+    try {
+      const pdfDoc = await PDFDocument.create();
+      const page = pdfDoc.addPage([612, 472]);
+      const { width, height } = page.getSize();
+      const fontSize = 19.2;
+      const textHeight = fontSize + 10;
+
+      page.drawText("Queue Management System by Tecnavis", {
+        x: width / 2 - 200,
+        y: height - 100,
+        size: 24,
+        color: rgb(0, 0, 0),
+      });
+
+      page.drawText(tokenNumber, {
+        x: width / 2 - 40,
+        y: height / 2,
+        size: 35,
+        color: rgb(0, 0, 0),
+      });
+
+      const pdfBytes = await pdfDoc.save();
+      const pdfBlob = new Blob([pdfBytes], { type: 'application/pdf' });
+      const a = document.createElement('a');
+      a.href = URL.createObjectURL(pdfBlob);
+      a.download = 'token.pdf';
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    } catch (error) {
+      console.error("Error generating PDF: ", error);
+    }
+  };
+
+  return (
+    <div className="flex flex-col min-h-dvh justify-center items-center">
+      <h2 className="font-semibold text-xl mb-6">Request Submitted Successfully</h2>
+      <p className="mb-6">Your token number is: {tokenNumber}</p>
+      <Button className="bg-[#6236F5] text-white" onClick={handleDownloadPDF}>Download Token</Button>
+      <Button className="bg-[#6236F5] text-white" onClick={() => navigate("/")}>Go Back Home</Button>
+    </div>
+  );
+}
