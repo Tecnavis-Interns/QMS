28d13ac5a95778ee800dc8122e00c474fec43349
Rameesha <rameeshasharin05@gmail.com>
2024-08-03 23:57:51 +0530

    update index.js

--- index.js
+++ index.js
@@ -31,10 +31,10 @@ const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, '..', 'dist')));
+app.use(express.static(__dirname));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Function to clear a collection
